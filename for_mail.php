1f3008b8ff945d032a0a1ded504d1f9b0f75fa77
zabi-hac <74953733+zabi-hac@users.noreply.github.com>
2021-11-14 19:55:26 +0530

    Create for_mail.php

---
+++ for_mail.php
@@ -0,0 +1,22 @@
+
+// $to      = 'shabeer@signroots.com';
+// $subject = 'subject';
+// $message = 'hello';
+// $headers = 'From: shabeer@signroots.com' . "\r\n" .
+//     'Reply-To: shabeer@signroots.com' . "\r\n" .
+//     'X-Mailer: PHP/' . phpversion();
+// mail($to, $subject, $message, $headers);
+ 
+
+// $headers = "From: webmaster@example.com\r\n";
+// $headers .= "Reply-To: shabeer@signroots.com\r\n";
+// $headers .= "Return-Path: shabeer@signroots.com\r\n";
+// // $headers .= "CC: sombodyelse@example.com\r\n";
+// // $headers .= "BCC: hidden@example.com\r\n";
+
+// if ( mail($to,$subject,$message,$headers) ) {
+//    echo "The email has been sent!";
+//    } else {
+//    echo "The email has failed!";
+//    }
+//  
